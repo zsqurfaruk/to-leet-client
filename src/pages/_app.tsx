c2e1a8c2c357ae9b10661bdee94328d930866039
farukphero <farukk6319@gmail.com>
2023-06-01 23:03:06 +0600

    available added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,19 +2,17 @@ import Footer from "@/components/Shared/Footer/Footer";
 import NavBar from "@/components/Shared/NavBar/NavBar";
 import ApiContext from "@/Context/ApiContext/ApiContext";
 import FilterInfo from "@/Context/FilterContext/FilterContext";
-import NamInfo from "@/Context/NameInfo";
 import PostInfo from "@/Context/PostStateContext/PostStateContext";
 import StateInfo from "@/Context/StateContext/StateContext";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import  { Toaster } from 'react-hot-toast';
+import { Toaster } from "react-hot-toast";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <FilterInfo>
       <PostInfo>
         <StateInfo>
-          <NamInfo>
           <ApiContext>
             <NavBar></NavBar>
             <div>
@@ -23,7 +21,6 @@ export default function App({ Component, pageProps }: AppProps) {
             <Toaster />
             <Footer></Footer>
           </ApiContext>
-          </NamInfo>
         </StateInfo>
       </PostInfo>
     </FilterInfo>
