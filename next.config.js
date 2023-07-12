f30440c21fde7a40017a6b0fea48d43d53fcc477
faruk <farukk6319@gmail.com>
2023-07-12 22:36:17 +0600

    pagination handle 12-7-23

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    optimizeCss: true,
+    optimizeImages: true,
+    workerThreads: true,
+    fs: false,
+  },
 };
 
 module.exports = nextConfig;
