f23aa8bee19b324d3ef7f16a90e2cd9ab2c7ad78
faruk <farukk6319@gmail.com>
2023-07-23 02:01:25 +0600

    23-7-23, share link solve and edit sign in up

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config, { webpack }) => {
-    config.optimization.minimize = false;
-    config.optimization.minimizer = [];
-    return config;
-  },
 };
 
 module.exports = nextConfig;
